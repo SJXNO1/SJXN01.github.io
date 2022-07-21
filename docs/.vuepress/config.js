5baa15a1619f23b7d3b41cb3d18f5f13716ec19d
家新 <66517539+SJXNO1@users.noreply.github.com>
2022-07-22 00:11:39 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,24 +11,6 @@ module.exports = {
         nav: [
             { text: 'External', link: 'https://google.com', target: '_self', rel: '' },
             { text: 'Guide', link: '/guide/', target: '_blank' }
-        ],
-        /*
-        sidebar: [
-            {
-                title: 'Group 1',   // 必要的
-                path: '/foo/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-                collapsable: false, // 可选的, 默认值是 true,
-                sidebarDepth: 1,    // 可选的, 默认值是 1
-                children: [
-                    '/'
-                ]
-            },
-            {
-                title: 'Group 2',
-                children: [ /* ... */],
-                initialOpenGroupIndex: -1 // 可选的, 默认值是 0
-            }
         ]
-    */
     }
 }
